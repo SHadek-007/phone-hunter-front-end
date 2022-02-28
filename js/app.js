5747d3068cea0c856ec09113b4997a3ccd31b245
SADEK HOSSAIN <shadeks16@gmail.com>
2022-03-01 00:12:25 +0600

    phone search added

--- js/app.js
+++ js/app.js
@@ -4,10 +4,24 @@ const loadAllPhones = () =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => loadDisplayPhones(data.data))
     searchBox.value = '';
 }
 const loadDisplayPhones = phones =>{
-    <div id="search-result" class="row row-cols-1 row-cols-md-3 g-4">
-    </div>
+    const phoneContainer = document.getElementById('search-result');
+    phones.forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card h-100">
+                <img class="w-50 mx-auto pt-3" src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body text-center ">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <h6 class="card-text">${phone.brand}</h6>
+                    <button  onclick="loadPhoneDetails(${phone.slug})" class="btn btn-primary">Details</button>
+                </div>
+            </div>
+            `;
+            phoneContainer.appendChild(div);
+    });
 }
